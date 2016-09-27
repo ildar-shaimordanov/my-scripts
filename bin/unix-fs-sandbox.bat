c9125493a776d51795b0ab62c0ca4e2ecaac0e8f
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2016-09-27 14:10:17 +0400

    rework installation of mandatory files in unix-fs-sandbox.bat

--- bin/unix-fs-sandbox.bat
+++ bin/unix-fs-sandbox.bat
@@ -18,7 +18,10 @@
 ::
 :: 2016
 ::
-:: Version 0.2 Beta.
+:: Version 0.3 Beta
+:: Rework installation of mandatory files.
+::
+:: Version 0.2 Beta
 :: It is implemented as the command line tool.
 ::
 ::
@@ -44,8 +47,11 @@
 :: -- uninstalling feature
 ::
 :: -- drive icon / label
-:: http://www.sevenforums.com/tutorials/65828-drive-icon-change.html
-:: http://www.sevenforums.com/tutorials/66118-drive-rename.html
+::    http://www.sevenforums.com/tutorials/65828-drive-icon-change.html
+::    http://www.sevenforums.com/tutorials/66118-drive-rename.html
+::
+:: -- options for creating user-specific or system-wide shortcuts to
+::    Desktop, Start Menu
 
 :: ========================================================================
 
@@ -70,18 +76,22 @@ set "sandbox-error="
 
 call :sandbox-parse-opts %*
 
+:: Bad option provided
 if defined sandbox-error (
 	call :sandbox-error "%sandbox-error%"
 	exit /b 1
 )
 
+:: --help or --version
 if errorlevel 1 exit /b 0
 
+:: Validate disk name
 for %%f in ( "%sandbox-disk%\." ) do if not "%%~dpf" == "%%~ff" (
 	call :sandbox-error "invalid disk name: %sandbox-disk%"
 	exit /b 1
 )
 
+:: Canonicalize disk name and path
 for %%f in ( "%sandbox-disk%" ) do set "sandbox-disk=%%~df"
 for %%f in ( "%sandbox-path%" ) do set "sandbox-path=%%~ff"
 
@@ -104,16 +114,7 @@ echo:Create mandatory directories
 call :sandbox-mkdir-hier "%sandbox-mandatory-dirs:;=" "%" || exit /b 1
 
 echo:Install mandatory files
-call :sandbox-write-file release "etc\sandbox-release" || exit /b 1
-for %%f in (
-	"etc\rc.bat"
-	"etc\init.d\vdisk.bat"
-	"etc\rc.d\rc.pause"
-	"etc\rc.d\rc.start"
-	"etc\rc.d\rc.stop"
-) do (
-	call :sandbox-write-file static "%%~f" || exit /b 1
-)
+call :sandbox-write-files "%~f0" || exit /b 1
 
 set "sandbox-reg-device=HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices"
 
@@ -254,24 +255,38 @@ goto :EOF
 
 :: ========================================================================
 
-:sandbox-write-file-static
-for /f "tokens=1,* delims= " %%a in ( '
-	findstr /l /b "::%~1" "%~f0" 
-' ) do (
-	echo:%%~b
+:sandbox-write-files
+setlocal enabledelayedexpansion
+
+set "sandbox-filename="
+
+for /f "delims=] tokens=1,*" %%r in ( '
+	find /n /v "" "%~1"
+' ) do for /f "tokens=1,2,*" %%a in ( 
+	"LINE %%~s" 
+) do if "%%~b" == "::FILE-BEGIN" (
+	set "sandbox-filename=%sandbox-path%\%%~c"
+	echo:Write file: !sandbox-filename!
+	(
+		for /f "tokens=*" %%f in ( "!sandbox-filename!" ) do if not exist "%%~dpf" md "%%~dpf"
+		type nul > "!sandbox-filename!"
+	) || exit /b 1
+) else if "%%~b" == "::FILE-END" (
+	if defined sandbox-readonly attrib +r "!sandbox-filename!"
+	set "sandbox-filename="
+) else if defined sandbox-filename (
+	>>"!sandbox-filename!" (
+		setlocal disabledelayedexpansion
+		if "%%~b" == "::FILE-CALL" (
+			call %%~c
+		) else (
+			echo:%%~s
+		)
+		endlocal
+	) || exit /b 1
 )
-goto :EOF
 
-:sandbox-write-file-release
-call :sandbox-write-file-static "%~1"
-echo:%sandbox-disk%;%sandbox-path%;%sandbox-name%
-goto :EOF
-
-:sandbox-write-file
-echo:Write file: "%sandbox-path%\%~2"
-call :sandbox-write-file-%~1 "%~2" > "%sandbox-path%\%~2"
-if errorlevel 1 exit /b 1
-if defined sandbox-readonly attrib +r "%sandbox-path%\%~2"
+endlocal
 goto :EOF
 
 :: ========================================================================
@@ -279,217 +294,220 @@ goto :EOF
 :: The following part of the file represents the contents of files that 
 :: will be installed as the part of the sandbox. The beginning of each 
 :: file is marked with "::FILE-BEGIN filename" and the end of the file is 
-:: marked with "::FILE-END". 
+:: marked with "::FILE-END". The special marker "::FILE-CALL command" is 
+:: used to put the result of execution of the specified command to the 
+:: result file.
 
 :: ========================================================================
 
 ::FILE-BEGIN etc\rc.bat
-::etc\rc.bat :: This file is part of UNIX FS SANDBOX
-::etc\rc.bat @echo off
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat setlocal
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat set "rc-etc=%~dp0rc.d"
-::etc\rc.bat set "rc-list="
-::etc\rc.bat set "rc-skip-pause="
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat if "%~1" == "skip-pause" (
-::etc\rc.bat 	set "rc-skip-pause=1"
-::etc\rc.bat 	shift /1
-::etc\rc.bat )
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat if "%~1" == "" (
-::etc\rc.bat 	>&2 echo:Usage: %~n0 {start^|stop}
-::etc\rc.bat 	goto :EOS
-::etc\rc.bat )
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat rem
-::etc\rc.bat rem Looking for run-lists
-::etc\rc.bat rem
-::etc\rc.bat dir /a-d "%rc-etc%\rc.list" >nul 2>&1
-::etc\rc.bat if not errorlevel 1 (
-::etc\rc.bat 	echo:Run-list "%rc-etc%\rc.list" found. Continue...
-::etc\rc.bat 	for /f "usebackq eol=#" %%a in ( "%rc-etc%\rc.list" ) do (
-::etc\rc.bat 		if "%~1" == "start" call :rc-list-append  "%%~a"
-::etc\rc.bat 		if "%~1" == "stop"  call :rc-list-prepend "%%~a"
-::etc\rc.bat 	)
-::etc\rc.bat ) else (
-::etc\rc.bat 	dir /a-d "%rc-etc%\rc.%~1" >nul 2>&1
-::etc\rc.bat 	if errorlevel 1 (
-::etc\rc.bat 		>&2 echo:Run-list "%rc-etc%\rc.%~1" not found. Exit...
-::etc\rc.bat 		goto :EOS
-::etc\rc.bat 	)
-::etc\rc.bat 	echo Processing "%rc-etc%\rc.%~1".
-::etc\rc.bat 	for /f "usebackq eol=#" %%a in ( "%rc-etc%\rc.%~1" ) do (
-::etc\rc.bat 		call :rc-list-append  "%%~a"
-::etc\rc.bat 	)
-::etc\rc.bat )
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat rem
-::etc\rc.bat rem Execute start/stop commands
-::etc\rc.bat rem
-::etc\rc.bat if not defined rc-list (
-::etc\rc.bat 	>&2 echo:Empty run-list. Nothing to execute...
-::etc\rc.bat 	goto :EOS
-::etc\rc.bat )
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat for %%a in ( 
-::etc\rc.bat 	"%rc-list:;=" "%" 
-::etc\rc.bat ) do if not "%%~a" == "" if not exist "%rc-etc%\..\init.d\%%~a.bat" (
-::etc\rc.bat 	>&2 echo:%%~a not found
-::etc\rc.bat ) else (
-::etc\rc.bat 	echo:%DATE% %TIME%: Executing "%%~a"...
-::etc\rc.bat 	call "%rc-etc%\..\init.d\%%~a.bat" "%~1"
-::etc\rc.bat )
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat rem
-::etc\rc.bat rem Make a pause (if it needs) and finish
-::etc\rc.bat rem
-::etc\rc.bat
-::etc\rc.bat :EOS
-::etc\rc.bat if not defined rc-skip-pause if exist "%rc-etc%\rc.pause" (
-::etc\rc.bat 	echo:To escape pausing remove the following file:
-::etc\rc.bat 	echo:"%rc-etc%\rc.pause".
-::etc\rc.bat 	pause
-::etc\rc.bat )
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat endlocal
-::etc\rc.bat goto :EOF
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat :rc-list-prepend
-::etc\rc.bat set "rc-list=%~1;%rc-list%"
-::etc\rc.bat goto :EOF
-::etc\rc.bat
-::etc\rc.bat
-::etc\rc.bat :rc-list-append
-::etc\rc.bat set "rc-list=%rc-list%;%~1"
-::etc\rc.bat goto :EOF
+:: This file is part of UNIX FS SANDBOX
+@echo off
+
+
+setlocal
+
+
+set "rc-etc=%~dp0rc.d"
+set "rc-list="
+set "rc-skip-pause="
+
+
+if "%~1" == "skip-pause" (
+	set "rc-skip-pause=1"
+	shift /1
+)
+
+
+if "%~1" == "" (
+	>&2 echo:Usage: %~n0 {start^|stop}
+	goto :EOS
+)
+
+
+rem
+rem Looking for run-lists
+rem
+dir /a-d "%rc-etc%\rc.list" >nul 2>&1
+if not errorlevel 1 (
+	echo:Run-list "%rc-etc%\rc.list" found. Continue...
+	for /f "usebackq eol=#" %%a in ( "%rc-etc%\rc.list" ) do (
+		if "%~1" == "start" call :rc-list-append  "%%~a"
+		if "%~1" == "stop"  call :rc-list-prepend "%%~a"
+	)
+) else (
+	dir /a-d "%rc-etc%\rc.%~1" >nul 2>&1
+	if errorlevel 1 (
+		>&2 echo:Run-list "%rc-etc%\rc.%~1" not found. Exit...
+		goto :EOS
+	)
+	echo Processing "%rc-etc%\rc.%~1".
+	for /f "usebackq eol=#" %%a in ( "%rc-etc%\rc.%~1" ) do (
+		call :rc-list-append  "%%~a"
+	)
+)
+
+
+rem
+rem Execute start/stop commands
+rem
+if not defined rc-list (
+	>&2 echo:Empty run-list. Nothing to execute...
+	goto :EOS
+)
+
+
+for %%a in ( 
+	"%rc-list:;=" "%" 
+) do if not "%%~a" == "" if not exist "%rc-etc%\..\init.d\%%~a.bat" (
+	>&2 echo:%%~a not found
+) else (
+	echo:%DATE% %TIME%: Executing "%%~a"...
+	call "%rc-etc%\..\init.d\%%~a.bat" "%~1"
+)
+
+
+rem
+rem Make a pause (if it needs) and finish
+rem
+
+:EOS
+if not defined rc-skip-pause if exist "%rc-etc%\rc.pause" (
+	echo:To escape pausing remove the following file:
+	echo:"%rc-etc%\rc.pause".
+	pause
+)
+
+
+endlocal
+goto :EOF
+
+
+:rc-list-prepend
+set "rc-list=%~1;%rc-list%"
+goto :EOF
+
+
+:rc-list-append
+set "rc-list=%rc-list%;%~1"
+goto :EOF
 ::FILE-END
 
 
 ::FILE-BEGIN etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat :: This file is part of UNIX FS SANDBOX
-::etc\init.d\vdisk.bat @echo off
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat setlocal
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat if not exist "%~dp0..\sandbox-release" (
-::etc\init.d\vdisk.bat 	>&2 echo:"%~dp0..\sandbox-release" not found
-::etc\init.d\vdisk.bat 	goto :EOS
-::etc\init.d\vdisk.bat )
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat for /f "usebackq eol=# delims=; tokens=1,2,*" %%a in (
-::etc\init.d\vdisk.bat 	"%~dp0..\sandbox-release"
-::etc\init.d\vdisk.bat ) do (
-::etc\init.d\vdisk.bat 	set "sandbox-disk=%%~a"
-::etc\init.d\vdisk.bat 	set "sandbox-path=%%~b"
-::etc\init.d\vdisk.bat 	set "sandbox-name=%%~c"
-::etc\init.d\vdisk.bat )
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat if not defined sandbox-disk (
-::etc\init.d\vdisk.bat 	>&2 echo:Sandbox disk not defined
-::etc\init.d\vdisk.bat 	goto :EOS
-::etc\init.d\vdisk.bat )
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat if not defined sandbox-path (
-::etc\init.d\vdisk.bat 	>&2 echo:Sandbox path not defined
-::etc\init.d\vdisk.bat 	goto :EOS
-::etc\init.d\vdisk.bat )
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat if "%~1" == "start" (
-::etc\init.d\vdisk.bat 	echo:Mapping vdisk %sandbox-disk% =^> %sandbox-path%
-::etc\init.d\vdisk.bat 	subst %sandbox-disk% "%sandbox-path%"
-::etc\init.d\vdisk.bat 	goto :EOS
-::etc\init.d\vdisk.bat )
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat if "%~1" == "stop" (
-::etc\init.d\vdisk.bat 	echo:Release vdisk %sandbox-disk% =^> %sandbox-path%
-::etc\init.d\vdisk.bat 	subst %sandbox-disk% /D
-::etc\init.d\vdisk.bat 	goto :EOS
-::etc\init.d\vdisk.bat )
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat if "%~1" == "status" (
-::etc\init.d\vdisk.bat 	subst | findstr /i /b "%sandbox-disk%"
-::etc\init.d\vdisk.bat 	goto :EOS
-::etc\init.d\vdisk.bat )
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat >&2 echo:Usage: %~n0 {start^|stop^|status}
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat
-::etc\init.d\vdisk.bat :EOS
-::etc\init.d\vdisk.bat endlocal
-::etc\init.d\vdisk.bat goto :EOF
+:: This file is part of UNIX FS SANDBOX
+@echo off
+
+
+setlocal
+
+
+if not exist "%~dp0..\sandbox-release" (
+	>&2 echo:"%~dp0..\sandbox-release" not found
+	goto :EOS
+)
+
+for /f "usebackq eol=# delims=; tokens=1,2,*" %%a in (
+	"%~dp0..\sandbox-release"
+) do (
+	set "sandbox-disk=%%~a"
+	set "sandbox-path=%%~b"
+	set "sandbox-name=%%~c"
+)
+
+if not defined sandbox-disk (
+	>&2 echo:Sandbox disk not defined
+	goto :EOS
+)
+
+if not defined sandbox-path (
+	>&2 echo:Sandbox path not defined
+	goto :EOS
+)
+
+
+if "%~1" == "start" (
+	echo:Mapping vdisk %sandbox-disk% =^> %sandbox-path%
+	subst %sandbox-disk% "%sandbox-path%"
+	goto :EOS
+)
+
+
+if "%~1" == "stop" (
+	echo:Release vdisk %sandbox-disk% =^> %sandbox-path%
+	subst %sandbox-disk% /D
+	goto :EOS
+)
+
+
+if "%~1" == "status" (
+	subst | findstr /i /b "%sandbox-disk%"
+	goto :EOS
+)
+
+
+>&2 echo:Usage: %~n0 {start^|stop^|status}
+
+
+:EOS
+endlocal
+goto :EOF
 ::FILE-END
 
 
 ::FILE-BEGIN etc\rc.d\rc.pause
-::etc\rc.d\rc.pause # This file is part of UNIX FS SANDBOX
-::etc\rc.d\rc.pause #
-::etc\rc.d\rc.pause # To escape pausing, remove this file
+## This file is part of UNIX FS SANDBOX
+#
+# To escape pausing, remove this file
 ::FILE-END
 
 
 ::FILE-BEGIN etc\rc.d\rc.list
-::etc\rc.d\rc.list # This file is part of UNIX FS SANDBOX
-::etc\rc.d\rc.list #
-::etc\rc.d\rc.list # There is list of names of scripts from the directory "etc/init.d" to be 
-::etc\rc.d\rc.list # executed while starting and stopping the sandbox. One name is per one 
-::etc\rc.d\rc.list # line. The order of the script execution depends on the current mode. 
-::etc\rc.d\rc.list # Wjhile starting, this list is assumed from top to bottom. 
-::etc\rc.d\rc.list # While stopping, this list is assumed from bottom to top. 
-::etc\rc.d\rc.list #
-::etc\rc.d\rc.list # If you need to change the order of execution, use two separate 
-::etc\rc.d\rc.list # corresponding lists:
-::etc\rc.d\rc.list # -- "etc/rc.d/rc.start" for starting
-::etc\rc.d\rc.list # -- "etc/rc.d/rc.stop" for stopping
-::etc\rc.d\rc.list vdisk
+## This file is part of UNIX FS SANDBOX
+#
+# There is list of names of scripts from the directory "etc/init.d" to be 
+# executed while starting and stopping the sandbox. One name is per one 
+# line. The order of the script execution depends on the current mode. 
+# Wjhile starting, this list is assumed from top to bottom. 
+# While stopping, this list is assumed from bottom to top. 
+#
+# If you need to change the order of execution, use two separate 
+# corresponding lists:
+# -- "etc/rc.d/rc.start" for starting
+# -- "etc/rc.d/rc.stop" for stopping
+vdisk
 ::FILE-END
 
 
 ::FILE-BEGIN etc\rc.d\rc.start
-::etc\rc.d\rc.start # This file is part of UNIX FS SANDBOX
-::etc\rc.d\rc.start #
-::etc\rc.d\rc.start # There is list of names of scripts from the directory "etc/init.d" to be 
-::etc\rc.d\rc.start # executed while starting the sandbox. One name is per one line. The order 
-::etc\rc.d\rc.start # of names corresponds to the order of their execution.
-::etc\rc.d\rc.start vdisk
+## This file is part of UNIX FS SANDBOX
+#
+# There is list of names of scripts from the directory "etc/init.d" to be 
+# executed while starting the sandbox. One name is per one line. The order 
+# of names corresponds to the order of their execution.
+vdisk
 ::FILE-END
 
 
 ::FILE-BEGIN etc\rc.d\rc.stop
-::etc\rc.d\rc.stop # This file is part of UNIX FS SANDBOX
-::etc\rc.d\rc.stop #
-::etc\rc.d\rc.stop # There is list of names of scripts from the directory "etc/init.d" to be 
-::etc\rc.d\rc.stop # executed while stopping the sandbox. One name is per one line. The order 
-::etc\rc.d\rc.stop # of names corresponds to the order of their execution.
-::etc\rc.d\rc.stop vdisk
+## This file is part of UNIX FS SANDBOX
+#
+# There is list of names of scripts from the directory "etc/init.d" to be 
+# executed while stopping the sandbox. One name is per one line. The order 
+# of names corresponds to the order of their execution.
+vdisk
 ::FILE-END
 
 
 ::FILE-BEGIN etc\sandbox-release
-::etc\sandbox-release # This file is part of UNIX FS SANDBOX
-::etc\sandbox-release #
-::etc\sandbox-release # This file contains the detailedd information regarding the current 
-::etc\sandbox-release # sandbox. It consists of the following fields separated with semicolon:
-::etc\sandbox-release # -- the virtual drive to which a path will be assigned;
-::etc\sandbox-release # -- the directory where the sandbox is installed;
-::etc\sandbox-release # -- the sandbox name.
+## This file is part of UNIX FS SANDBOX
+#
+# This file contains the detailedd information regarding the current 
+# sandbox. It consists of the following fields separated with semicolon:
+# -- the virtual drive to which a path will be assigned;
+# -- the directory where the sandbox is installed;
+# -- the sandbox name.
+::FILE-CALL echo:%sandbox-disk%;%sandbox-path%;%sandbox-name%
 ::FILE-END
 
 :: ========================================================================
