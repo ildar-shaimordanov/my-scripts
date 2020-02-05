74b63a2cc9f29942e0297a07a6ea99a59d4611f1
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2020-02-06 01:45:37 +0300

    mimic version for SUDO

---
+++ bin/sudo.bat
@@ -0,0 +1,12 @@
+:: SUDO [command [options]]
+::
+:: Mimic to the unix command with the same name
+:: Allow to run a command with the elevated privileges
+@echo off
+
+if "%~1" == "" (
+	call "%~f0" cmd.exe "/s /k pushd \"%cd%\""
+	goto :EOF
+)
+
+PowerShell -Command Start-Process "%~1" -Args '%2 %3 %4 %5 %6 %7 %8 %9' -Verb RunAs
