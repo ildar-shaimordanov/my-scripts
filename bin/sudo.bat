a311586c80c5874d1b05437c461569ac50fc7b68
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2020-02-06 17:56:59 +0300

    add show usage and check requirements

--- bin/sudo.bat
+++ bin/sudo.bat
@@ -1,8 +1,28 @@
-:: SUDO [command [options]]
+::NAME
 ::
-:: Mimic to the unix command with the same name
-:: Allow to run a command with the elevated privileges
+::    sudo - execute a command with the elevated privileges
+::
+::SYNOPSIS
+::
+::    sudo
+::    sudo COMMAND [OPTIONS]
+::
+::DESCRIPTION
+::
+::sudo allows to run a command with optional arguments with the elevated
+::privileges. By default, if no any options specified, it runs the
+::command interpreter. The command can be any of binary executables,
+::batch scripts or documents supposed to be open.
+::
+::REQUIREMENTS
+::
+::The script requires PowerShell to be installed on the system.
 @echo off
+
+if /i "%~1" == "/?" goto :print_usage
+if /i "%~1" == "/h" goto :print_usage
+
+call :check_reqs || goto :EOF
 
 setlocal
 
@@ -23,6 +43,20 @@ call :sudo /c start /d "%CD%" %*
 
 goto :EOF
 
+
 :sudo
 endlocal && powershell -Command Start-Process "cmd.exe" -Args '%*' %SUDO_HIDDEN% -Verb RunAs
+goto :EOF
+
+
+:print_usage
+for /f "tokens=* delims=:" %%s in ( 'findstr "^::" "%~f0"' ) do echo:%%s
+goto :EOF
+
+
+:check_reqs
+for %%p in ( powershell.exe ) do if "%%~$PATH:p" == "" (
+	echo:%%~p is required>&2
+	exit /b 1
+)
 goto :EOF
