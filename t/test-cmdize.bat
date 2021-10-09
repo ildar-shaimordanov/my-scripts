ad807836271472208950ec7d4b0310504e32b536
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2021-10-09 20:49:49 +0300

    improve test for cmdize.bat

--- t/test-cmdize.bat
+++ t/test-cmdize.bat
@@ -1,5 +1,9 @@
 @echo off
 
+setlocal
+
+set "test_dir=%TEMP%"
+
 echo:==== COMMON TESTS ====
 
 for /f %%x in ( '"%~dp0..\cmdize.bat" /L' ) do call :test_ext "%%~x"
@@ -10,7 +14,7 @@ call :test_ext .js "/e node"
 call :test_ext .pl "/e cmdonly"
 call :test_ext .py "/e short"
 
-del z.*
+del "%test_dir%\z.*"
 
 goto :EOF
 
@@ -19,16 +23,14 @@ echo:
 echo:==== %~1 [%~2] ====
 echo:
 
-> "z%~1" (
+> "%test_dir%\z%~1" (
 	if "%~1" == ".vbs" echo:Option Explicit
-	if "%~1" == ".wsf" (
-		echo:^<?xml?^>^<job^>^<script^>WScript.Echo^("Hello"^)^</script^>^</job^>
-	)
+	if "%~1" == ".wsf" echo:^<?xml?^>^<job^>^<script^>WScript.Echo^("Hello"^)^</script^>^</job^>
 )
 
-call "%~dp0..\cmdize.bat" %~2 "z%~1"
+call "%~dp0..\cmdize.bat" %~2 "%test_dir%\z%~1"
 
-more < "z.bat"
+more < "%test_dir%\z.bat"
 
 echo:
 :pause
