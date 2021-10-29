ea45d41c5382f28cfb965978f4224fa9e39c6c07
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2021-10-29 14:23:31 +0300

    cmdize: update documentation

--- cmdize.bat
+++ cmdize.bat
@@ -20,17 +20,17 @@
 ::H>
 ::H>This tool converts a script into a batch file allowing to use the script like regular programs and batch scripts without invoking an executable engine explicitly and just typing the script name without extension. The resulting batch file is placed next to the original script.
 ::H>
-::H>The new file consist of the body of the script prepended with the special header (or prolog) being the "polyglot" and having some tricks to be a valid code both for the batch and original script.
+::H>The new file consist of the body of the script prepended with the special header (or prolog) being the *polyglot* and having some tricks to be a valid code both for the batch and original script.
 ::H>
 ::H>This tool is pure batch file. So there is limitation in processing files having Byte Order Mark (BOM). For example, it fail with high probability while processing a unicode encoded WSF-file with XML declaration.
 ::H>
-::H>The *engine* term stands for the executable running the script. Not for all languages it's applicable. Depending the language, the engine can be set to any, none or one of predefined values. `/E DEFAULT` is the special engine that resets any previously set engines to the default value.
+::H>The *engine* term stands for the executable running the script. Not for all languages it's applicable. Depending the language, the engine can be set to any, none or one of predefined values. `/E DEFAULT` is the special engine that resets any previously set engines to the default value. The same result can be received with `/E ""`.
 ::H>
 ::H>For WSF-scripts the engine is one of `CSCRIPT` and `WSCRIPT`. If XML declaration is presented (in the form like `<?xml...?>`), it must be in the most beginning of the file. Otherwise error is reported and the script is not cmdized.
 ::H>
 ::H>For JavaScript/JScript it can be one of `CSCRIPT`, `WSCRIPT` (for JScript5+), `CCHAKRA`, `WCHAKRA` (for JScript9 or Chakra) or any valid command with options to enable running NodeJS, ChakraCore, Rhino and so on (for example, `node`, `ch`, `java -jar rhino.jar`, respectively).
 ::H>
-::H>For VBScript there is choice of either `CSCRIPT` or `WSCRIPT`. If the script implements the statement `Option Explicit`, then it is commented to avoid the compilation error. The `/W` option creates the alternative runner embedding the script into a WSF-file. In this case that statement is not commented and left as is.
+::H>For VBScript there is choice from either `CSCRIPT` or `WSCRIPT`. If the script implements the statement `Option Explicit`, then it is commented to avoid the compilation error. The `/W` option creates the alternative runner embedding the script into a WSF-file. In this case that statement is not commented and left as is.
 ::H>
 ::H>For Perl `/E CMDONLY` is the only applicable value. It's fake engine that is used for creating the pure batch file for putting it with the original script in PATH.
 ::H>
@@ -137,10 +137,10 @@ goto :cmdize_loop_begin
 
 ::D>## .js
 ::D>
-::D>* `CSCRIPT` for `cscript //nologo //e:javascript` (default)
-::D>* `WSCRIPT` for `wscript //nologo //e:javascript`
-::D>* `CCHAKRA` for `cscript //nologo //e:{16d51579-a30b-4c8b-a276-0ff4dc41e755}`
-::D>* `WCHAKRA` for `wscript //nologo //e:{16d51579-a30b-4c8b-a276-0ff4dc41e755}`
+::D>* `/E CSCRIPT` for `cscript //nologo //e:javascript` (default)
+::D>* `/E WSCRIPT` for `wscript //nologo //e:javascript`
+::D>* `/E CCHAKRA` for `cscript //nologo //e:{16d51579-a30b-4c8b-a276-0ff4dc41e755}`
+::D>* `/E WCHAKRA` for `wscript //nologo //e:{16d51579-a30b-4c8b-a276-0ff4dc41e755}`
 ::D>
 ::D>Unfortunately, no easy way to wrap JScript9 into WSF.
 ::D>
@@ -172,13 +172,15 @@ goto :EOF
 
 ::D>## .vbs
 ::D>
-::D>* `CSCRIPT` for `cscript //nologo //e:vbscript` (default)
-::D>* `WSCRIPT` for `wscript //nologo //e:vbscript`
+::D>Pure VBScript within a batch file:
 ::D>
-::D>With `/W` it is (with some specialties for WSF):
+::D>* `/E CSCRIPT` for `cscript //nologo //e:vbscript` (default)
+::D>* `/E WSCRIPT` for `wscript //nologo //e:vbscript`
 ::D>
-::D>* `CSCRIPT` for `cscript //nologo` (default)
-::D>* `WSCRIPT` for `wscript //nologo`
+::D>With `/W` it's WSF within a batch file (with some specialties for WSF):
+::D>
+::D>* `/E CSCRIPT` for `cscript //nologo` (default)
+::D>* `/E WSCRIPT` for `wscript //nologo`
 ::D>
 ::D>* http://www.dostips.com/forum/viewtopic.php?p=33882#p33882
 ::D>* http://www.dostips.com/forum/viewtopic.php?p=32485#p32485
@@ -245,7 +247,7 @@ goto :EOF
 
 ::D>## .pl
 ::D>
-::D>The document below gives more details about `pl2bat.bat` and `runperl.bat`. In fact, those scripts are full-featured prototypes for this script. By default it acts as the first one but without supporting old DOSs. With the `/W` option it creates the tiny batch acting similar to `runperl.bat`.
+::D>The document below gives more details about `pl2bat.bat` and `runperl.bat`. In fact, those scripts are full-featured prototypes for this script. By default it acts as the first one but without supporting old DOSs. With the `/E CMDONLY` option it creates the tiny batch acting similar to `runperl.bat`.
 ::D>
 ::D>* https://perldoc.perl.org/perlwin32
 ::D>
@@ -277,7 +279,7 @@ goto :EOF
 
 ::D>## .ps1
 ::D>
-::D>Very-very-very complicated case. It's impossible to implement hybrid. And too hard to implement chimera. The resulting batch store its filename and passed arguments in two environment variables, `PS1_FILE` and `PS1_ARGS`, respectively. Then it invokes powershell and tries to restore arguments, reads the file and invokes it. Also it is powered to continue working with STDIN properly. Powershell has two (at least known for me) ways to invoke another code: Invoke-Expression and invoke ScriptBlock. Both have their advandages and disadvantages. By default, Invoke-Expression is used. To give the users a choice between both, non-empty value in `PS1_ISB` enables ScriptBlock invocation.
+::D>Very-very-very complicated case. It's impossible to implement a pure hybrid. And too hard to implement a chimera. The resulting batch stores its filename and passed arguments in two environment variables `PS1_FILE` and `PS1_ARGS`, respectively. Then it invokes powershell which tries to restore arguments, reads the file and invokes it. Also it is powered to continue working with STDIN properly. Powershell has two (at least known for me) ways to invoke another code: Invoke-Expression and invoke ScriptBlock. Both have their advandages and disadvantages. By default, Invoke-Expression is used. To give the users a choice between both, non-empty value in `PS1_ISB` enables ScriptBlock invocation.
 ::D>
 ::D>* http://blogs.msdn.com/b/jaybaz_ms/archive/2007/04/26/powershell-polyglot.aspx
 ::D>* http://stackoverflow.com/a/2611487/3627676
@@ -345,6 +347,8 @@ goto :EOF
 ::D>## .wsf
 ::D>
 ::D>Hybridizing WSF the script looks for the XML declaration and makes it valid for running as batch. Also weird and undocumented trick with file extensions (`%~f0?.wsf`) is used to insist WSH to recognize the batch file as the WSF scenario. Honestly, the resulting file stops being well-formed XML file. However WSH chews it silently.
+::D>
+::D>BOM fails cmdizing.
 ::D>
 ::D>Assuming the original XML declaration is as follows:
 ::D>
@@ -422,7 +426,7 @@ goto :EOF
 
 ::D>## .php
 ::D>
-::D>PHP is supposed to be used as a scripting language in Web. So to avoid possible conflicts with paths to dynamic libraries and to suppress HTTP headers, we use two options "-n" and "-q", respectively.
+::D>PHP is supposed to be used as a scripting language in Web. So to avoid possible conflicts with paths to dynamic libraries and to suppress HTTP headers, we use two options `-n` and `-q`, respectively.
 ::D>
 :cmdize.php
 call :print-prolog "php -n -q" "<?php/* :" "*/ ?>"
@@ -453,14 +457,16 @@ goto :EOF
 ::G>
 ::G>Prints different parts of the documentation.
 ::G>
+::G>Arguments
+::G>
+::G>* `%1` - the marker
+::G>
+::G>Available markers:
+::G>
 ::G>* `U` - to print usage
 ::G>* `UH` - to print help (the `/H` option)
 ::G>* `UHD` - to print help in details (the `/HELP` option)
 ::G>* `UHDG` - to print full help (the `/HELP-FULL` option)
-::G>
-::G>Arguments
-::G>
-::G>* `%1` - the marker
 ::G>
 :print-usage
 for /f "tokens=1,* delims=>" %%a in ( 'findstr /r "^::[%~1]>" "%~f0"' ) do echo:%%b
@@ -470,7 +476,7 @@ goto :EOF
 
 ::G>## `:warn`
 ::G>
-::G>A common use subroutine for printing arguments to STDERR.
+::G>A common use subroutine for displaying warnings to STDERR.
 ::G>
 ::G>Arguments
 ::G>
@@ -484,7 +490,7 @@ goto :EOF
 
 ::G>## `:print-script-wsf-bat`
 ::G>
-::G>The purpose of this subroutine is to unify hybridizing a particular file as a WSF-file. It creates a temporary WSF-file with the content of the original file within and then hybridize it.
+::G>The purpose of this subroutine is to unify hybridizing a particular file as a WSF-file. It creates a temporary WSF-file with the content of the original file within and then hybridizes it.
 ::G>
 ::G>To this moment it is used only once - for VBScript.
 ::G>
@@ -521,7 +527,7 @@ goto :EOF
 
 ::G>## `:print-prolog`
 ::G>
-::G>This internal subroutine is a workhorse. It creates prologs. Depending on the passed arguments it produces different prologs.
+::G>This internal subroutine is a real workhorse. It creates prologs. Depending on the passed arguments it produces different prologs.
 ::G>
 ::G>Arguments
 ::G>
--- doc/cmdize.md
+++ doc/cmdize.md
@@ -50,17 +50,17 @@ Converts a script into a batch file.
 
 This tool converts a script into a batch file allowing to use the script like regular programs and batch scripts without invoking an executable engine explicitly and just typing the script name without extension. The resulting batch file is placed next to the original script.
 
-The new file consist of the body of the script prepended with the special header (or prolog) being the "polyglot" and having some tricks to be a valid code both for the batch and original script.
+The new file consist of the body of the script prepended with the special header (or prolog) being the *polyglot* and having some tricks to be a valid code both for the batch and original script.
 
 This tool is pure batch file. So there is limitation in processing files having Byte Order Mark (BOM). For example, it fail with high probability while processing a unicode encoded WSF-file with XML declaration.
 
-The *engine* term stands for the executable running the script. Not for all languages it's applicable. Depending the language, the engine can be set to any, none or one of predefined values. `/E DEFAULT` is the special engine that resets any previously set engines to the default value.
+The *engine* term stands for the executable running the script. Not for all languages it's applicable. Depending the language, the engine can be set to any, none or one of predefined values. `/E DEFAULT` is the special engine that resets any previously set engines to the default value. The same result can be received with `/E ""`.
 
 For WSF-scripts the engine is one of `CSCRIPT` and `WSCRIPT`. If XML declaration is presented (in the form like `<?xml...?>`), it must be in the most beginning of the file. Otherwise error is reported and the script is not cmdized.
 
 For JavaScript/JScript it can be one of `CSCRIPT`, `WSCRIPT` (for JScript5+), `CCHAKRA`, `WCHAKRA` (for JScript9 or Chakra) or any valid command with options to enable running NodeJS, ChakraCore, Rhino and so on (for example, `node`, `ch`, `java -jar rhino.jar`, respectively).
 
-For VBScript there is choice of either `CSCRIPT` or `WSCRIPT`. If the script implements the statement `Option Explicit`, then it is commented to avoid the compilation error. The `/W` option creates the alternative runner embedding the script into a WSF-file. In this case that statement is not commented and left as is.
+For VBScript there is choice from either `CSCRIPT` or `WSCRIPT`. If the script implements the statement `Option Explicit`, then it is commented to avoid the compilation error. The `/W` option creates the alternative runner embedding the script into a WSF-file. In this case that statement is not commented and left as is.
 
 For Perl `/E CMDONLY` is the only applicable value. It's fake engine that is used for creating the pure batch file for putting it with the original script in PATH.
 
@@ -92,10 +92,10 @@ More description, more links, more details about implementation in this section.
 
 ## .js
 
-* `CSCRIPT` for `cscript //nologo //e:javascript` (default)
-* `WSCRIPT` for `wscript //nologo //e:javascript`
-* `CCHAKRA` for `cscript //nologo //e:{16d51579-a30b-4c8b-a276-0ff4dc41e755}`
-* `WCHAKRA` for `wscript //nologo //e:{16d51579-a30b-4c8b-a276-0ff4dc41e755}`
+* `/E CSCRIPT` for `cscript //nologo //e:javascript` (default)
+* `/E WSCRIPT` for `wscript //nologo //e:javascript`
+* `/E CCHAKRA` for `cscript //nologo //e:{16d51579-a30b-4c8b-a276-0ff4dc41e755}`
+* `/E WCHAKRA` for `wscript //nologo //e:{16d51579-a30b-4c8b-a276-0ff4dc41e755}`
 
 Unfortunately, no easy way to wrap JScript9 into WSF.
 
@@ -110,20 +110,22 @@ The following two links show my first steps in direction to create this script.
 
 ## .vbs
 
-* `CSCRIPT` for `cscript //nologo //e:vbscript` (default)
-* `WSCRIPT` for `wscript //nologo //e:vbscript`
+Pure VBScript within a batch file:
 
-With `/W` it is (with some specialties for WSF):
+* `/E CSCRIPT` for `cscript //nologo //e:vbscript` (default)
+* `/E WSCRIPT` for `wscript //nologo //e:vbscript`
 
-* `CSCRIPT` for `cscript //nologo` (default)
-* `WSCRIPT` for `wscript //nologo`
+With `/W` it's WSF within a batch file (with some specialties for WSF):
+
+* `/E CSCRIPT` for `cscript //nologo` (default)
+* `/E WSCRIPT` for `wscript //nologo`
 
 * http://www.dostips.com/forum/viewtopic.php?p=33882#p33882
 * http://www.dostips.com/forum/viewtopic.php?p=32485#p32485
 
 ## .pl
 
-The document below gives more details about `pl2bat.bat` and `runperl.bat`. In fact, those scripts are full-featured prototypes for this script. By default it acts as the first one but without supporting old DOSs. With the `/W` option it creates the tiny batch acting similar to `runperl.bat`.
+The document below gives more details about `pl2bat.bat` and `runperl.bat`. In fact, those scripts are full-featured prototypes for this script. By default it acts as the first one but without supporting old DOSs. With the `/E CMDONLY` option it creates the tiny batch acting similar to `runperl.bat`.
 
 * https://perldoc.perl.org/perlwin32
 
@@ -134,7 +136,7 @@ The document below gives more details about `pl2bat.bat` and `runperl.bat`. In f
 
 ## .ps1
 
-Very-very-very complicated case. It's impossible to implement hybrid. And too hard to implement chimera. The resulting batch store its filename and passed arguments in two environment variables, `PS1_FILE` and `PS1_ARGS`, respectively. Then it invokes powershell and tries to restore arguments, reads the file and invokes it. Also it is powered to continue working with STDIN properly. Powershell has two (at least known for me) ways to invoke another code: Invoke-Expression and invoke ScriptBlock. Both have their advandages and disadvantages. By default, Invoke-Expression is used. To give the users a choice between both, non-empty value in `PS1_ISB` enables ScriptBlock invocation.
+Very-very-very complicated case. It's impossible to implement a pure hybrid. And too hard to implement a chimera. The resulting batch stores its filename and passed arguments in two environment variables `PS1_FILE` and `PS1_ARGS`, respectively. Then it invokes powershell which tries to restore arguments, reads the file and invokes it. Also it is powered to continue working with STDIN properly. Powershell has two (at least known for me) ways to invoke another code: Invoke-Expression and invoke ScriptBlock. Both have their advandages and disadvantages. By default, Invoke-Expression is used. To give the users a choice between both, non-empty value in `PS1_ISB` enables ScriptBlock invocation.
 
 * http://blogs.msdn.com/b/jaybaz_ms/archive/2007/04/26/powershell-polyglot.aspx
 * http://stackoverflow.com/a/2611487/3627676
@@ -155,6 +157,8 @@ Very-very-very complicated case. It's impossible to implement hybrid. And too ha
 ## .wsf
 
 Hybridizing WSF the script looks for the XML declaration and makes it valid for running as batch. Also weird and undocumented trick with file extensions (`%~f0?.wsf`) is used to insist WSH to recognize the batch file as the WSF scenario. Honestly, the resulting file stops being well-formed XML file. However WSH chews it silently.
+
+BOM fails cmdizing.
 
 Assuming the original XML declaration is as follows:
 
@@ -178,7 +182,7 @@ further it becomes:
 
 ## .php
 
-PHP is supposed to be used as a scripting language in Web. So to avoid possible conflicts with paths to dynamic libraries and to suppress HTTP headers, we use two options "-n" and "-q", respectively.
+PHP is supposed to be used as a scripting language in Web. So to avoid possible conflicts with paths to dynamic libraries and to suppress HTTP headers, we use two options `-n` and `-q`, respectively.
 
 ## .jl
 
@@ -194,18 +198,20 @@ This section discovers all guts of the hybridization.
 
 Prints different parts of the documentation.
 
+Arguments
+
+* `%1` - the marker
+
+Available markers:
+
 * `U` - to print usage
 * `UH` - to print help (the `/H` option)
 * `UHD` - to print help in details (the `/HELP` option)
 * `UHDG` - to print full help (the `/HELP-FULL` option)
 
-Arguments
-
-* `%1` - the marker
-
 ## `:warn`
 
-A common use subroutine for printing arguments to STDERR.
+A common use subroutine for displaying warnings to STDERR.
 
 Arguments
 
@@ -213,7 +219,7 @@ Arguments
 
 ## `:print-script-wsf-bat`
 
-The purpose of this subroutine is to unify hybridizing a particular file as a WSF-file. It creates a temporary WSF-file with the content of the original file within and then hybridize it.
+The purpose of this subroutine is to unify hybridizing a particular file as a WSF-file. It creates a temporary WSF-file with the content of the original file within and then hybridizes it.
 
 To this moment it is used only once - for VBScript.
 
@@ -233,7 +239,7 @@ Arguments
 
 ## `:print-prolog`
 
-This internal subroutine is a workhorse. It creates prologs. Depending on the passed arguments it produces different prologs.
+This internal subroutine is a real workhorse. It creates prologs. Depending on the passed arguments it produces different prologs.
 
 Arguments
 
