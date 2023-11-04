409e4ec9b6e443b739a2db0ef3e03b95f105a8dc
Ildar Shaimordanov <ildar.shaimordanov@gmail.com>
2023-11-04 18:05:53 +0300

    add the postgresql describe-all monster

---
+++ sql/postgresql-describe-all/README.md
@@ -0,0 +1,36 @@
+# Preamble
+
+It's my attempt to extract all the project-related tables, columns and their descriptions.
+
+I'm not postgresql guru, so I spent a lot of hours during a few days to invent the approach giving the applicable result.
+
+# Explanation
+
+There is final version. You can find it as a separate file below. I found and fixed all my errors from previous attempts and made some improvements in sorting tables and columns names correctly (a table name first, then its column names and so on). Also I added querying schemas and their descriptions. It works as expected. I am not 100% sure in its performance and optimality but I am not going use it frequently.
+
+The `where table_schema like '%sm'` clause is used to query my project-related stuff. It should be adjusted to specify uniquely your project stuff only. Probably, depending on the model of your project, the last `where`-clause should be modified. For example, if all your tables were created within the `public` schema, the query can look like:
+
+```sql
+where table_schema = 'public'
+```
+
+or a little bit more complicated (to skip any service tables used in PostgreSQL)
+
+```sql
+where table_schema not in ( 'information_schema', 'pg_catalog', 'pg_toast', 'public' )
+```
+
+and so on.
+
+For sure, you can modify all of them for your needs to filter excess data and query needful information only.
+
+# See also
+
+* https://gist.github.com/alexanderlz/7302623
+* https://www.sqlshack.com/learn-sql-inner-join-vs-left-join/
+* https://postgrespro.ru/docs/postgresql/9.5/catalogs
+  * https://postgrespro.ru/docs/postgresql/9.5/catalog-pg-attribute
+  * https://postgrespro.ru/docs/postgresql/9.5/catalog-pg-class
+  * https://postgrespro.ru/docs/postgresql/9.5/catalog-pg-description
+  * https://postgrespro.ru/docs/postgresql/9.5/view-pg-tables
+
---
+++ sql/postgresql-describe-all/describe-all.sql
@@ -0,0 +1,63 @@
+with all_descriptions as (
+  select
+      pn.nspname as table_schema
+    , null::text as table_name
+    , null::int8 as n
+    , null::text as column_name
+    , null::text as column_type
+    , null::text as nullable
+    , null::text as default
+    , pd.description as comment
+  from pg_catalog.pg_namespace pn
+  left join pg_catalog.pg_description pd
+    on pd.objoid = pn.oid
+union all
+  select
+      t.table_schema
+    , t.table_name
+    , null::int8 as n
+    , null::text as column_name
+    , null::text as column_type
+    , null::text as nullable
+    , null::text as default
+    , pd.description as comment
+  from pg_catalog.pg_class pc
+  left join information_schema.tables t
+    on t.table_name = pc.relname
+  left join pg_catalog.pg_description pd
+    on pd.objoid = pc.oid
+    and pd.objsubid = 0
+  where pc.relkind = 'r'
+union all
+  select
+      c.table_schema
+    , c.table_name
+    , pa.attnum as n
+    , pa.attname as column_name
+    , case
+      when data_type = 'numeric' and numeric_precision is not null
+      then data_type || ' (' || numeric_precision || ', ' || numeric_scale || ')'
+      when character_maximum_length is not null
+      then data_type || ' (' || character_maximum_length || ')'
+      else data_type
+      end as column_type
+    , c.is_nullable as nullable
+    , c.column_default as default
+    , pd.description as comment
+  from pg_catalog.pg_class pc
+  left join pg_catalog.pg_attribute pa
+    on pa.attrelid = pc.oid
+    and not pa.attisdropped
+    and pa.attnum > 0
+  left join pg_catalog.pg_description pd
+    on pd.objoid = pc.oid
+    and pd.objsubid = pa.attnum
+  left join information_schema.columns c
+    on c.table_name = pc.relname
+    and c.ordinal_position = pa.attnum
+  where pc.relkind = 'r'
+)
+select *
+from all_descriptions ad
+where table_schema like '%sm' -- project-related
+order by table_schema, table_name nulls first, n nulls first
