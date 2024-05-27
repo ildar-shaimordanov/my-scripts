9dde1d0cbd149428e7c81ebdcb8c7ce47df0eb92
Ildar Shaimordanov <ildar-shaimordanov@users.noreply.github.com>
2024-05-28 01:15:30 +0300

    internal changes

--- postgresql/pg-describe/pg-describe-pub.sh
+++ postgresql/pg-describe/pg-describe-pub.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 ./pg-describe.sh \
-"where table_schema = 'public'" \
-"and table_name is distinct from 'alembic_version'"
+"where table_schema = 'public' and table_name is distinct from 'alembic_version'"
