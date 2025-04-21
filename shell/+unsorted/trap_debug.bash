4176321b3fd5bddb1cd4721be2226509042787ec
Ildar Shaimordanov <ildar-shaimordanov@users.noreply.github.com>
2025-04-21 09:44:46 +0300

    Update trap_debug.bash

--- shell/+unsorted/trap_debug.bash
+++ shell/+unsorted/trap_debug.bash
@@ -9,6 +9,12 @@
 # trap trap_debug DEBUG
 # ```
 #
+# How to use it without function
+#
+# ```shell
+# trap 'printf "# $LINENO: $BASH_COMMAND" >&2' DEBUG
+# ```
+#
 # Based on the idea published here:
 # https://www.cyberforum.ru/blogs/1976142/10017.html
 #
