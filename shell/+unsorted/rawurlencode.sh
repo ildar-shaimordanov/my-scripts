d9561974766ba4de43837963425045a84e30affe
Ildar Shaimordanov <ildar-shaimordanov@users.noreply.github.com>
2025-10-20 00:19:51 +0300

    add rawurlencode.sh

---
+++ shell/+unsorted/rawurlencode.sh
@@ -0,0 +1,23 @@
+# +++
+#
+# Urlencode any string on pure POSIX shell with no grep/sed/jq.
+#
+# rawurlencode "P@ssw0rd" # results to "P%40ssw0rd"
+#
+# This implementation is based on these two brilliant ideas:
+# * https://stackoverflow.com/a/10660730
+# * https://blog.dnmfarrell.com/post/how-to-split-a-string-in-posix-shell/
+#
+# ---
+
+rawurlencode() (
+	LANG=C
+	OPTIND=1
+	while getopts ':' arg "-$1"
+	do
+		case "$OPTARG" in
+		[-_.~a-zA-Z0-9]	) printf '%s' "$OPTARG" ;;
+		*		) printf '%%%02x' "'${OPTARG:-:}" ;;
+		esac
+	done
+)
