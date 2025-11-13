4f54fa1698eb28d1a0efe4fcb1cdf19c5ed44b99
Ildar Shaimordanov <ildar.shaimordanov@gmail.com>
2025-11-13 17:30:31 +0300

    implement three versions for get_ipv4

--- shell/+unsorted/get_ipv4.sh
+++ shell/+unsorted/get_ipv4.sh
@@ -7,7 +7,13 @@
 #     $ get_ipv4 lo
 #     127.0.0.1
 #
+# There are three implementations. See for details in source.
+#
 # ---
+
+# Version 1
+# It uses `ip link show`, `ip -o -4 addr show` and builtin `read`
+# reading data in subshell.
 
 get_ipv4() {
 	ip link show "$1" >/dev/null \
@@ -20,3 +26,43 @@ get_ipv4() {
 		echo "$ip"
 	}
 }
+
+# Version 2
+# It uses `ip -o -4 addr show` only storing data to internal variable,
+# and builtin `echo | read`. Everything is runing in subshell.
+
+get_ipv4() (
+	get_ipv4="$( ip -o -4 addr show dev "$1" )" || return 1
+
+	[ ${#get_ipv4} -gt 0 ] || {
+		>&2 echo "No IPv4 configured for \"$1\"."
+		return 1
+	}
+
+	# shellcheck disable=SC2034
+	echo "$get_ipv4" | {
+		IFS="$IFS/" read -r id name family ip rest
+		echo "$ip"
+	}
+)
+
+# Version 2
+# It uses `ip -o -4 addr show` only storing data to internal variable,
+# and builtin `read` reading data from heredoc. Everything is runing
+# in subshell.
+
+get_ipv4_() (
+	get_ipv4="$( ip -o -4 addr show dev "$1" )" || return 1
+
+	[ ${#get_ipv4} -gt 0 ] || {
+		>&2 echo "No IPv4 configured for \"$1\"."
+		return 1
+	}
+
+	# shellcheck disable=SC2034
+	IFS="$IFS/" read -r id name family ip rest <<-!
+	$get_ipv4
+	!
+
+	echo "$ip"
+)
