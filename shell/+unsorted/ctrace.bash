109508fa41a20e02161a3f8bc8819a13c787c13a
Ildar Shaimordanov <ildar.shaimordanov@gmail.com>
2025-11-13 17:32:25 +0300

    simplify implementation for ctrace

--- shell/+unsorted/ctrace.bash
+++ shell/+unsorted/ctrace.bash
@@ -1,16 +1,22 @@
 # +++
 #
-# Print the full stack of execution contexts
+# Print a stack of execution contexts
+#
+# Usage:
+#     ctrace [EXPR]
+#
+# Without EXPR, returns a full stacktrace. When EXPR is defined, it
+# means how many call frames to skip before printing a stacktrace.
 #
 # ---
 
 ctrace() {
-	set -- 0 ; while caller "$1" ; do set -- $(( $1+1 )) ; done
+	set -- "${1:-0}" ; while caller "$1" ; do set -- $(( $1+1 )) ; done
 }
 
 # Insane version working without loops
 #
 # ctrace() {
-# 	set -- 0 'caller $1 || return 0; eval "set -- $(( $1+1 )) \"\$2\"; $2"'
+# 	set -- "${1:-0}" 'caller $1 || return 0; eval "set -- $(( $1+1 )) \"\$2\"; $2"'
 # 	eval "$2"
 # }
