00c01f63c5bb889448dea16b4ad79de7ee83ef63
Ildar Shaimordanov <ildar.shaimordanov@gmail.com>
2025-11-21 19:04:00 +0300

    add insane version for rep.sh

--- shell/+unsorted/rep.sh
+++ shell/+unsorted/rep.sh
@@ -5,10 +5,11 @@
 # * `$1` - string
 # * `$2` - number or repetition
 #
-# There are three implementations:
+# There are four implementations:
 #
 # * bash only, full of bashisms
 # * POSIX-compliant
+# * POSIX-compliant, insane use of awk
 # * POSIX-compliant, awful
 #
 # Example:
@@ -31,6 +32,17 @@ rep() {
 rep() {
 	printf "%${2}s" \
 	| awk -v s="$1" -v RS='' -v ORS='' '{ gsub(" ", s) } 1'
+}
+
+# POSIX-compliant, insane use of awk
+
+rep() {
+	awk \
+	-v char="$1" \
+	-v width="$2" \
+	-v RS='' \
+	-v ORS='' \
+	'BEGIN { s = sprintf("%" width "s", ""); gsub(" ", char, s); print s }'
 }
 
 # POSIX-compliant, awful
